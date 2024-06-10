9c23a93b7e4a630fca1c7b3ad7c8be1ca01c164f
Jiří Grygerek <nextikczcz@gmail.com>
2024-06-10 20:33:46 +0200

    add support for custom registration for Skript elements. add support for basic custom Logger -> HoradricLogger.java

---
+++ src/main/java/com/coffee/sashie/horadriccube/utils/ElementType.java
@@ -0,0 +1,11 @@
+package com.coffee.sashie.horadriccube.utils;
+
+public enum ElementType {
+    EXPRESSIONS,
+    EVENTS,
+    EFFECTS,
+    SECTIONS,
+    STRUCTURES,
+    FUNCTIONS,
+    CONDITIONS
+}
